dd6e19f6c98b315338c3dd076abc6998b7b26b21
DongShangXian <dantesx2012@gmail.com>
2015-09-24 14:30:07 +0800

    初始化

---
+++ SXTableViewManager/SXTableViewManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SXTableViewManager
+//
+//  Created by dongshangxian on 15/9/24.
+//  Copyright © 2015年 Sankuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SXTableViewManager/SXTableViewManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SXTableViewManager
+//
+//  Created by dongshangxian on 15/9/24.
+//  Copyright © 2015年 Sankuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
